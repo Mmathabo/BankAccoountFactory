43fcacca57abdd2db1b2d866f519520a87d2d07c
Ruimin <zhangruimincn@gmail.com>
2014-02-11 10:40:39 +0200

    An account object is returned based on the type of account that is passed when the account is passed as a parameter

--- src/AccountFactory.java
+++ src/AccountFactory.java
@@ -1,3 +1,15 @@
 public class AccountFactory {
 
+    BankAccount getAccountType(String accountType) {
+        if (accountType == null) {
+            return null;
+
+        } else if (accountType.equalsIgnoreCase("SavingsAccount")) {
+            return new SavingsAccount();
+        } else if (accountType.equalsIgnoreCase("ChequeAccount")) {
+            return new ChequeAccount();
+        }
+        return null;
+
+    }
 }
